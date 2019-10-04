9621a79bde113c98e8a815e7b6dd5f831bc5225e
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2019-10-04 12:31:24 +0100

    Add unit test project, consolidate test-package references

---
+++ SCSqlBatch.Tests/BatchLogicTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace SCSqlBatch.Tests
+{
+    [TestFixture]
+    public class BatchLogicTests
+    {
+    }
+}
---
+++ SCSqlBatch.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SCSqlBatch.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SharpCloud Software Limited")]
+[assembly: AssemblyProduct("SCSqlBatch.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2019 SharpCloud Software Limited")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb5fdee1-10f6-49e3-b04f-85227028b590")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
