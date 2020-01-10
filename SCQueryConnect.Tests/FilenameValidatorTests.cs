e939c9029ec86bf2c10759d2446142fbaa8958e3
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2020-01-10 16:32:08 +0000

    Add blank test project; update/consolidate test dependencies

---
+++ SCQueryConnect.Tests/FilenameValidatorTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace SCQueryConnect.Tests
+{
+    [TestFixture]
+    public class FilenameValidatorTests
+    {
+    }
+}
---
+++ SCQueryConnect.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SCQueryConnect.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SharpCloud Software Limited")]
+[assembly: AssemblyProduct("SCQueryConnect.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2020 SharpCloud Software Limited")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be98ba99-0a9c-454e-be8c-77cc4e76bc04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
