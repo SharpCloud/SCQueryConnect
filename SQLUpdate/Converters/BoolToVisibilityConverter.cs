b8879a320a5587865e63a90e650581586eceb7f7
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2020-01-30 16:10:39 +0000

    Add folder/connection icons

---
+++ SQLUpdate/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SCQueryConnect.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visibility = Visibility.Collapsed;
+
+            if (value is bool boolValue &&
+                parameter is string paramString)
+            {
+                var success = bool.TryParse(paramString, out var boolParam);
+
+                if (success && boolValue == boolParam)
+                {
+                    visibility = Visibility.Visible;
+                }
+            }
+
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
