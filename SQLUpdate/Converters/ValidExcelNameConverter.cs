937ad524dc8721799f2a7dd28b8fbfb9f5fb2a6e
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2020-02-18 16:03:57 +0000

    Bug fix: Access file names being suffixed with Excel file extension

--- SQLUpdate/Converters/ValidExcelNameConverter.cs
+++ SQLUpdate/Converters/ValidExcelNameConverter.cs
@@ -8,35 +8,55 @@ namespace SCQueryConnect.Converters
 {
     public class ValidExcelNameConverter : IValueConverter
     {
-        private readonly HashSet<string> _validFileExtensions
-            = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-        {
-            ".xls",
-            ".xlsb",
-            ".xlsm",
-            ".xlsx"
-        };
+        private const string AccessKey = "Access";
+        private const string ExcelKey = "Excel";
+
+        private readonly Dictionary<string, HashSet<string>> _validFileExtensions
+            = new Dictionary<string, HashSet<string>>
+            {
+                [ExcelKey] = new HashSet<string>
+                {
+                    ".xls",
+                    ".xlsb",
+                    ".xlsm",
+                    ".xlsx"
+                },
+                [AccessKey] = new HashSet<string>
+                {
+                    ".accdb"
+                }
+            };
+
+        private readonly Dictionary<string, string> _defaultExtensions
+            = new Dictionary<string, string>
+            {
+                [ExcelKey] = ".xlsx",
+                [AccessKey] = ".accdb"
+            };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return GetValidFilename(value);
+            return GetValidFilename(value, parameter);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return GetValidFilename(value);
+            return GetValidFilename(value, parameter);
         }
 
-        private string GetValidFilename(object value)
+        private string GetValidFilename(object value, object parameter)
         {
-            if (value is string filename)
+            if (value is string filename && parameter is string key)
             {
                 var ext = Path.GetExtension(filename);
-                var isValid = _validFileExtensions.Contains(ext);
+                
+                var isValid =
+                    _validFileExtensions.ContainsKey(key) &&
+                    _validFileExtensions[key].Contains(ext);
 
                 var suffix = isValid
                     ? string.Empty
-                    : ".xlsx";
+                    : _defaultExtensions[key];
 
                 var toReturn = $"{filename}{suffix}";
                 return toReturn;
