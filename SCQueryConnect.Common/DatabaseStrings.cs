ca72dd57f1b8fac7b89cea2a45d50c655c194f15
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2020-02-19 13:04:47 +0000

    Add DB types for generating CData batch files

--- SCQueryConnect.Common/DatabaseStrings.cs
+++ SCQueryConnect.Common/DatabaseStrings.cs
@@ -5,13 +5,17 @@ namespace SCQueryConnect.Common
 {
     public static class DatabaseStrings
     {
+        public const string Access = "ACCESS";
+        public const string Excel = "EXCEL";
         public const string MsAdeAccess = "MS_ADE_ACCESS";
         public const string MsAdeExcel = "MS_ADE_EXCEL";
+        public const string MsAdeSharePointList = "MS_ADE_SHAREPOINT_LIST";
+        public const string MsAdeSharpCloudExcel = "MS_ADE_SHARPCLOUD_EXCEL";
         public const string Sql = "SQL";
         public const string Odbc = "ODBC";
         public const string OleDb = "OLEDB";
-        public const string MsAdeSharePointList = "MS_ADE_SHAREPOINT_LIST";
-        public const string MsAdeSharpCloudExcel = "MS_ADE_SHARPCLOUD_EXCEL";
+        public const string SharePointList = "SHAREPOINT_LIST";
+        public const string SharpCloudExcel = "SHARPCLOUD_EXCEL";
 
         public const string ExcelFileKey = "Excel File";
         public const string DataSourceKey = "Data Source";
@@ -19,12 +23,16 @@ namespace SCQueryConnect.Common
         public static Dictionary<DatabaseType, string> TypeStringMapping
             = new Dictionary<DatabaseType, string>
             {
+                [DatabaseType.Access] = Access,
+                [DatabaseType.Excel] = Excel,
                 [DatabaseType.MsAdeAccess] = MsAdeAccess,
                 [DatabaseType.MsAdeExcel] = MsAdeExcel,
+                [DatabaseType.MsAdeSharePointList] = MsAdeSharePointList,
+                [DatabaseType.MsAdeSharpCloudExcel] = MsAdeSharpCloudExcel,
                 [DatabaseType.Sql] = Sql,
                 [DatabaseType.Odbc] = Odbc,
-                [DatabaseType.MsAdeSharePointList] = MsAdeSharePointList,
-                [DatabaseType.MsAdeSharpCloudExcel] = MsAdeSharpCloudExcel
+                [DatabaseType.SharePointList] = SharePointList,
+                [DatabaseType.SharpCloudExcel] = SharpCloudExcel
             };
 
         public static Dictionary<string, DatabaseType> StringTypeMapping
